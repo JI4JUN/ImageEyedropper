695c2641edcf3c6d8861069570e38370b5fb5304
qiujiajun <545509146@qq.com>
2024-07-03 18:10:49 +0800

    chore: reinitialize the project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ImageEyeDropper from './components/ImageEyeDropper/ImageEyeDropper.vue';
+</script>
+
+<template>
+    <ImageEyeDropper></ImageEyeDropper>
+</template>
---
+++ src/components/ImageEyeDropper/ImageEyeDropper.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div
+        class="flex max-h-[479px] w-[327px] flex-col items-start rounded-md bg-[#1c1c1c] p-[10px_16px_16px_16px] shadow-[4px_4px_4px_0_rgba(0,0,0,0.25)]"
+    >
+        <div
+            class="text-[12px] font-normal not-italic text-[rgba(255,255,255,0.8)]"
+        >
+            取色参考图，直接点击下方图片进行取色
+        </div>
+        <div
+            class="mb-[18px] mt-[14px] cursor-pointer rounded border border-solid border-[rgba(255,255,255,0.22)]"
+        >
+            <!-- <UploadReference
+                class="flex flex-row items-center justify-center"
+                @reference-loaded="handleReferenceLoaded"
+            > -->
+            <div
+                class="flex h-[32px] w-[186px] flex-row items-center justify-center gap-[10px]"
+            >
+                <!-- <Svg
+                        class="reUpload-btn-icon"
+                        :name="Svgs.RecolorEyeDropperUpload"
+                    ></Svg> -->
+                <div
+                    class="text-[12px] font-semibold not-italic text-[rgba(255,255,255,0.9)]"
+                >
+                    重新上传参考图
+                </div>
+            </div>
+            <!-- </UploadReference> -->
+        </div>
+        <div ref="imageWindowRef" class="relative self-center rounded-md">
+            <div
+                id="canvasWrapper"
+                ref="canvasWrapperRef"
+                class="absolute left-1/2 top-1/2 box-border origin-[0_0] cursor-none"
+                :style="{ transform: `translate(-50%,-50%)` }"
+            ></div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
